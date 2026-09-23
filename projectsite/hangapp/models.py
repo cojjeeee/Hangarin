2ac1223fb50de41d27c62ecf776c2611f54bb62c
cojjeeee <gi686273@gmail.com>
2026-09-23 08:03:19 +0800

    Models, Faker, Admin, Server setup

--- projectsite/hangapp/models.py
+++ projectsite/hangapp/models.py
@@ -1,3 +1,94 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Priority(BaseModel):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Priority"
+        verbose_name_plural = "Priorities"
+
+    def __str__(self):
+        return self.name
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Task(BaseModel):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=50,
+        choices=[
+        ("Pending", "Pending"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+    ],
+        default="Pending",
+    )
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name="tasks"
+    )
+
+    priority = models.ForeignKey(
+        Priority,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="tasks"
+    )
+
+    def __str__(self):
+        return self.title
+
+class SubTask(BaseModel):
+
+    parent_task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="subtasks"
+    )
+    title = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=50,
+        choices=[
+        ("Pending", "Pending"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+    ],
+        default="Pending"
+    )
+
+    def __str__(self):
+        return self.title
+
+class Note(BaseModel):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="notes"
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Note for {self.task.title}"
+
